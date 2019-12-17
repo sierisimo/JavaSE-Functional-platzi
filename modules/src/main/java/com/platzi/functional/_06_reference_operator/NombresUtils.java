870bf77757fef31eb8feb0033ca779cc102874e7
Sinuhe Jaime <sierisimo@gmail.com>
2019-12-17 16:39:57 -0500

    Referencias referencias y mas referencias

---
+++ modules/src/main/java/com/platzi/functional/_06_reference_operator/NombresUtils.java
@@ -0,0 +1,20 @@
+package com.platzi.functional._06_reference_operator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NombresUtils {
+    public static void main(String[] args) {
+        List<String> profesores = getList("Nicolas", "Juan", "Zulema");
+
+        Consumer<String> printer = text -> System.out.println(text);
+        profesores.forEach(printer);
+        System.out.println("//////////");
+        profesores.forEach(System.out::println);
+    }
+
+    static <T> List<T> getList(T... elements){
+        return Arrays.asList(elements);
+    }
+}
