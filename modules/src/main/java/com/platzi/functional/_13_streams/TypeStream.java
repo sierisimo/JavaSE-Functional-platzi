39c4539684c18abcbd6f8fe2533bc313b366cbb2
Sinuhe Jaime <sierisimo@gmail.com>
2019-12-18 15:56:07 -0500

    Usando Collectors para convertir Streams

--- modules/src/main/java/com/platzi/functional/_13_streams/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_13_streams/TypeStream.java
@@ -1,13 +1,17 @@
 package com.platzi.functional._13_streams;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
     public static void main(String[] args) {
         IntStream infiniteStream = IntStream.iterate(0, x -> x + 1);
-        infiniteStream.limit(1000)
-                .parallel()
+        List<Integer> numbersList = infiniteStream.limit(1000)
                 .filter(x -> x % 2 == 0)
-                .forEach(System.out::println);
+                .boxed()
+                .collect(Collectors.toList());
+
+
     }
 }
